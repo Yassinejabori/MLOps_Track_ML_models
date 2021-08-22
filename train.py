b674b3ed28777e5281824f64b4d709702ee1e99e
Yassinejabori <74773233+Yassinejabori@users.noreply.github.com>
2021-08-22 16:09:40 +0100

    try QDA

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import json
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
-
+ 
 df = pd.read_csv("data_processed.csv")
 
 #### Get features ready to model! 
